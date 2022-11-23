4f2541fd88a1db0607528e263dec2ea12b6f07e1
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-11-23 16:32:54 +0100

    Implelemted FizzBuzz script selection

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-/* Scrivi un programma che stampi in console i numeri da 1 a 100: */
 for (let i = 1; i <= 100; i++ ) {
-    console.log(i)
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
